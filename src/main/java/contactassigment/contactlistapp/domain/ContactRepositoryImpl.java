1e8483e6d64a321803f3bbc27ba14b7e3b045db6
Ramesh <rameshs24@gmail.com>
2023-08-27 15:06:06 +1000

    Added GitHubActions - v2

--- src/main/java/contactassigment/contactlistapp/domain/ContactRepositoryImpl.java
+++ src/main/java/contactassigment/contactlistapp/domain/ContactRepositoryImpl.java
@@ -1,6 +1,5 @@
 package contactassigment.contactlistapp.domain;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,8 +8,6 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import org.owasp.esapi.ESAPI;
-import org.owasp.esapi.codecs.OracleCodec;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
