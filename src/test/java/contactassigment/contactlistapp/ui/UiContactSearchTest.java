e916d73fc9098936cf7fa52c7f248be426d40603
Ramesh <rameshs24@gmail.com>
2023-08-30 08:41:34 +1000

    Added Playwright UI Tests - Changed to Headless

--- src/test/java/contactassigment/contactlistapp/ui/UiContactSearchTest.java
+++ src/test/java/contactassigment/contactlistapp/ui/UiContactSearchTest.java
@@ -37,7 +37,7 @@ public class UiContactSearchTest {
     @BeforeAll
     static void launchBrowser() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
 
         try {
             Files.createDirectories(path);
