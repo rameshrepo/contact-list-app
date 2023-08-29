7c468b87dd112fd4c9416f3296da3c3e7b3473f7
Ramesh <rameshs24@gmail.com>
2023-08-30 08:32:57 +1000

    Added Playwright UI Tests

---
+++ src/test/java/contactassigment/contactlistapp/ui/UiContactSearchTest.java
@@ -0,0 +1,98 @@
+package contactassigment.contactlistapp.ui;
+
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class UiContactSearchTest {
+
+    @LocalServerPort
+    int randomPort;
+
+    // Shared between all tests in this class.
+    static Playwright playwright;
+    static Browser browser;
+
+    // New instance for each test method.
+    BrowserContext context;
+    Page page;
+
+    private static File targetClassesDir = new File(UiContactSearchTest.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+    private static Path path = Paths.get(targetClassesDir.getParent() + "/site/");
+
+    @BeforeAll
+    static void launchBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+
+        try {
+            Files.createDirectories(path);
+        } catch (IOException e) {
+            System.err.println("Failed to create directory!" + e.getMessage());
+        }
+    }
+
+    @AfterAll
+    static void closeBrowser() {
+        playwright.close();
+    }
+
+    @BeforeEach
+    void createContextAndPage() {
+        context = browser.newContext();
+        page = context.newPage();
+    }
+
+    @AfterEach
+    void closeContext() {
+        context.close();
+    }
+
+    @DisplayName("Test to check home page is displayed ")
+    @Test
+    void shouldShowWelcomePageOnStartUp() {
+        page.navigate("http://localhost:"+randomPort);
+        assertEquals("http://localhost:" + randomPort +"/", page.url());
+    }
+
+    @DisplayName("Test to check contact is displayed with Edit button for search result with single contact ")
+    @Test
+    void shouldDisplayContactsForMatchingSearchCriteriaAndEdit() {
+        page.navigate("http://localhost:"+randomPort+"/contacts");
+        page.getByLabel("FirstName").fill("Sophie");
+        page.getByText("Search").click();
+        assertEquals("Sophie", page.getByLabel("FirstName").inputValue());
+        assertEquals("Klein", page.getByLabel("LastName").inputValue());
+        assertEquals("Australian Agent for International Students", page.getByLabel("Organisation Name").inputValue());
+        assertEquals("11111111111", page.getByLabel("Organisation ABN").inputValue());
+        assertEquals("31/12/2022 00:00:00", page.getByLabel("Created").inputValue());
+        assertThat(page.getByRole(AriaRole.BUTTON).and (page.getByTitle("Edit")));
+        page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get(path + "/singleContact.png")));
+    }
+
+    @DisplayName("Test to check multiple contact is displayed for search result with more than one contact ")
+    @Test
+    void shouldDisplayContactsForMatchingSearchCriteria() {
+        page.navigate("http://localhost:"+ randomPort + "/contacts");
+        page.getByLabel("LastName").fill("B");
+        page.getByText("Search").click();
+        page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get(path + "/multipleContact.png")));
+        assertThat(page.getByRole(AriaRole.BUTTON).and (page.getByTitle("View")));
+    }
+
+
+}
--- src/test/java/contactassigment/contactlistapp/web/ContactControllerIntegrationTest.java
+++ src/test/java/contactassigment/contactlistapp/web/ContactControllerIntegrationTest.java
@@ -1,6 +1,9 @@
 package contactassigment.contactlistapp.web;
 
+import contactassigment.contactlistapp.domain.Contact;
 import contactassigment.contactlistapp.domain.ContactRepository;
+import contactassigment.contactlistapp.domain.Organisation;
+import contactassigment.contactlistapp.dto.ContactDTO;
 import contactassigment.contactlistapp.dto.ContactSearchCriteriaDTO;
 import contactassigment.contactlistapp.service.ContactServiceImpl;
 import contactassigment.contactlistapp.service.OrganisationServiceImpl;
@@ -15,6 +18,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Date;
 
 import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.equalTo;
@@ -94,5 +99,91 @@ public class ContactControllerIntegrationTest {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.forwardedUrl("/WEB-INF/jsp/contact/list.jsp"))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("contacts"));
+    }
+
+    @DisplayName("Test to return view and data for the given id")
+    @Test
+    public void testGetMethodReturnContactForAGivenId() throws Exception {
+
+        // given - precondition or setup
+        Integer id = 1;
+
+        //when and then
+        mockMvc.perform(MockMvcRequestBuilders.get("/contacts/{id}", id))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.forwardedUrl("/WEB-INF/jsp/contact/view.jsp"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("contact"))
+                .andExpect(MockMvcResultMatchers.model().attribute("contact", hasProperty("id", equalTo(1))));
+    }
+
+    @DisplayName("Test to return edit screen for the given id")
+    @Test
+    public void testGetMethodReturnEditContactForAGivenId() throws Exception {
+
+        // given - precondition or setup
+        Integer id = 1;
+
+        //when and then
+        mockMvc.perform(MockMvcRequestBuilders.get("/contacts/{id}/edit", id))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.forwardedUrl("/WEB-INF/jsp/contact/edit.jsp"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("contact"))
+                .andExpect(MockMvcResultMatchers.model().attribute("contact", hasProperty("id", equalTo(1))));
+    }
+
+    @DisplayName("Test to return edit if update has errors")
+    @Test
+    public void testPostMethod() throws Exception {
+
+        // given - precondition or setup
+        Organisation organisation = new Organisation();
+        organisation.setId(3);
+        organisation.setName("THE AUSTRALIAN");
+        organisation.setAbn(33333333333L);
+
+        Date currentDate = new Date();
+
+        Contact contact = new Contact();
+        contact.setId(8);
+        contact.setFirstName("Nick");
+        contact.setLastName("NameTooLongMoreThanThirtyCharacters");
+        contact.setOrganisation(organisation);
+        contact.setCreatedAt(currentDate);
+
+        ContactDTO contactDTO = ContactDTO.createBy(contact);
+
+        //when and then
+        mockMvc.perform(MockMvcRequestBuilders.post("/contacts").flashAttr("contact",contactDTO))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.forwardedUrl("/WEB-INF/jsp/contact/edit.jsp"))
+                .andExpect(MockMvcResultMatchers.model().hasErrors());
+    }
+
+    @DisplayName("Test to return list if update has successful")
+    @Test
+    public void testPostMethodUpdatesContact() throws Exception {
+
+        // given - precondition or setup
+        Organisation organisation = new Organisation();
+        organisation.setId(3);
+        organisation.setName("THE AUSTRALIAN");
+        organisation.setAbn(33333333333L);
+
+        Date currentDate = new Date();
+
+        Contact contact = new Contact();
+        Integer contactId = 8;
+        contact.setId(contactId);
+        contact.setFirstName("Nick");
+        contact.setLastName("Jack");
+        contact.setOrganisation(organisation);
+        contact.setCreatedAt(currentDate);
+
+        ContactDTO contactDTO = ContactDTO.createBy(contact);
+
+        //when and then
+        mockMvc.perform(MockMvcRequestBuilders.post("/contacts").flashAttr("contact",contactDTO))
+                .andExpect(MockMvcResultMatchers.status().isFound())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/contacts/"+contactId));
     }
 }
